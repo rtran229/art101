d51deda24b2888355c0032351404fa0aa92eab92
rtran229 <ptran35@ucsc.edu>
2025-04-21 20:47:10 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,12 +1,36 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab04: PseudoCoding
+// Author: Julie Tran
+// Date: 4/21/25
 
-// Constants
+//START
+//Initialize deck
+//Shuffle deck
+//Deal two cards to the player.
+//Deal two cards to the dealer.
+//Display the player's hand.
+//Display one dealer card (hide the other)
+//WHILE player wants to hit AND player total < 21
+  //  Deal one card to the player.
+  //  Display the updated hand.
+//IF player total > 21
+  //  Display "Player busts – Dealer wins
+  //  END GAME
+//Reveal the dealer's hidden card.
+//WHILE dealer total < 17
+  //  Deal one card to the dealer.
+  //  Display updated dealer hand
+//IF dealer total > 21
+  //  Display "Dealer busts – Player wins"
+//ELSE
+  //  Compare the player's total to the dealer's total.
+  //  IF player total > dealer total
+      //  Display "Player wins"
+    //ELSE IF player total < dealer total
+      //  Display "Dealer wins"
+    //ELSE
+      //  Display "Push – It's a tie"
+//END GAME 
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
   // return results;
