efcbc7833155485990072b1bc007d537b33ba97a
rtran229 <ptran35@ucsc.edu>
2025-04-24 21:35:22 -0700

    hi

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab05.js - Data Types and Variables
+// Author: Julie Tran
+// Date: April 24 2025
 
-// Constants
+//Variables
+make = "Toyota";
+model = "C-HR";
+color = "white";
+year = "2022";
 
-// Functions
+//Calculate
+age = 2025 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
