46098597d0b872e5da66f4a03aff03cc60bf5e3c
rtran229 <ptran35@ucsc.edu>
2025-05-19 22:04:38 -0700

    mrow

---
+++ lab11/js/lab.js
@@ -0,0 +1,22 @@
+// Lab11.js - JavaScript Events and Forms
+// Author: Julie Tran
+// Date: May 19 2025
+
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
+}
+
+// click listener for button
+$("#submit").click(function(){
+});
+
+// get value of input field
+const userName = $("#user-name").val();
+
+// now let's sort it
+userNameSorted = sortString(userName);
+
+// append a new div to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
