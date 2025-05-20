5387f1847147b6df810d3b6b6df00e91050a71fc
rtran229 <ptran35@ucsc.edu>
2025-05-19 22:13:45 -0700

    asda

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -10,13 +10,13 @@ function sortString(inputString) {
 
 // click listener for button
 $("#submit").click(function(){
+    // get value of input field
+    const userName = $("#user-name").val();
+
+    // now let's sort it
+    userNameSorted = sortString(userName);
+
+    // append a new div to our output div
+    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
 });
 
-// get value of input field
-const username = $("#username").val();
-
-// now let's sort it
-usernameSorted = sortString(username);
-
-// append a new div to our output div
-$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
