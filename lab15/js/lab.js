ea476a2cf205adfa8bf612cc6201391f17fbd192
rtran229 <ptran35@ucsc.edu>
2025-06-02 22:19:50 -0700

    fixed the entire thing

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -3,23 +3,34 @@
 // Date: June 2 2025
 
 //Doc ready
-$(document).ready(function() {
-  //Clicking button
-  $("#activate").click(function() {
-    //API call
-    $.ajax({
-      url: "https://pokeapi.co/api/v2/pokedex/2/",
-      type: "GET",  
-      dataType: "json",
+$(document).ready(function () {
+      $("#activate").click(function () {
+        $.ajax({
+          url: "https://pokeapi.co/api/v2/pokedex/2/",
+          type: "GET",
+          dataType: "json",
 
-      success: function(data) {
-        console.log(data);
-      },
+          success: function (data) {
+            console.log(data); 
+            // View full API data
+            
+            // List 10 pokemons
+            if (data && data.pokemon_entries) {
+              let outputHTML = "<ol>";
+              for (let i = 0; i < 10; i++) {
+                outputHTML += "<li>" + data.pokemon_entries[i].pokemon_species.name + "</li>";
+              }
+              outputHTML += "</ol>";
+              $("#output").html(outputHTML);
+            } else {
+              $("#output").html("<p class='error'>Unexpected data format.</p>");
+            }
+          },
 
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.log("Error:", textStatus, errorThrown);
-        $("#output").append("<p class='error'> Looks like something is wrong.</p>");
-      }
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error:", textStatus, errorThrown);
+            $("#output").append("<p class='error'>Looks like something is wrong.</p>");
+          }
+        });
+      });
     });
-  });
-});
